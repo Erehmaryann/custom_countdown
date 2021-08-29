515b5b41b3ed1fe0007ecd3279e8d07892a2f079
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-29 16:26:46 -0700

    selected the 1st item returned from the split method

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const countdownForm = document.querySelector("#countdownForm");
 const dateEl = document.querySelector("#date-picker");
 
 // Set date Input Minimum with Today's Date
-const today = new Date().toISOString().split("T");
+const today = new Date().toISOString().split("T")[0];
 console.log(today);
