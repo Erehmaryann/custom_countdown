700a05c608af9835078a95d4025a7f4806b5bfa3
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-29 15:52:26 -0700

    Selected the 2nd element needed in the HTML page and assigned it to a variable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // Select all the elements needed in the HTML page
 // and assign them to a variable
 const inputContainer = document.querySelector("#input-container");
+const countdownForm = document.querySelector("#countdownForm");
