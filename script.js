1da5b8665e578f1b5bc3a3df96b7a98258042ffa
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-29 19:05:09 -0700

    declared a global var for hour

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let countdownValue = Date;
 
 const second = 1000;
 const minute = second * 60;
+const hour = minute * 60;
 
 // Set date Input Minimum with Today's Date
 const today = new Date().toISOString().split("T")[0]; //split("T") returns an array of two items
